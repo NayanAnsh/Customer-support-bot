4ca55ce336c5acc4b95815de9314e398793a566e
nayan <nayanansh@gmail.com>
2025-10-15 20:20:45 +0530

    Delete temp.py

--- backend/temp.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-    # Configure your API key
-genai.configure(api_key="AIzaSyCT0xiv-7bAx02mCNx7qSn92VV6WOHXjdM")
-
-for m in genai.list_models():
-    if "generateContent" in m.supported_generation_methods:
-        print(m.name)
